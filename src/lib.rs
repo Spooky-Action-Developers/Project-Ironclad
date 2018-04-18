9c6a93de1a5cb426bc54f8d4d8d97a6b9775aba9
Evan Conley <econmang@gmail.com>
2018-04-17 22:33:12 -0700

    Added ability to list tables in default region and list tables by specified region. Added helper function to get region specified with user input. This allows for easier input and parsing of information. Further, created list_tables_default() and list_tables_region() to allow user to list tables from default and specified region. In order to use this function, logic of parsed data was altered in mainl. When list is called, it checks if region flag is set and then error checks it to make sure they entered a correct region. If it is, it creates region variable and lists tables associated with it. Else, it output list of correct inputs to be used.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate rusoto_credential;
 
 pub mod tables {
     use rusoto_core::region::Region;
-    use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput,DescribeTableInput};
+    use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
 
     pub fn list_tables_default() -> () {
         // First grabbing user credentials from .aws/credentials file
@@ -30,16 +30,39 @@ pub mod tables {
         }
     }
 
-    pub fn list_table_region() -> () {
-    //First grabbing user credentials from .aws/credentials file
-    let client = DynamoDbClient::simple(Region::UsWest2);
+    pub fn get_region(reg: &str) -> Option<Region> {
+        match reg {
+            "apnortheast1" =>  {return Some(Region::ApNortheast1)},
+            "apnortheast2" =>  {return Some(Region::ApNortheast2)},
+            "apsout1"      =>  {return Some(Region::ApSouth1)},
+            "apsoutheast1" =>  {return Some(Region::ApSoutheast1)},
+            "apsoutheast2" =>  {return Some(Region::ApSoutheast2)},
+            "cacentral1"   =>  {return Some(Region::CaCentral1)},
+            "eucentral1"   =>  {return Some(Region::EuCentral1)},
+            "euwest1"      =>  {return Some(Region::EuWest1)},
+            "euwest2"      =>  {return Some(Region::EuWest2)},
+            "euwest3"      =>  {return Some(Region::EuWest3)},
+            "saeast1"      =>  {return Some(Region::SaEast1)},
+            "useast1"      =>  {return Some(Region::UsEast1)},
+            "useast2"      =>  {return Some(Region::UsEast2)},
+            "uswest1"      =>  {return Some(Region::UsWest1)},
+            "uswest2"      =>  {return Some(Region::UsWest2)},
+            "usgovwest1"   =>  {return Some(Region::UsGovWest1)},
+            "cnnorth1"     =>  {return Some(Region::CnNorth1)},
+            "cnnorthwest1" =>  {return Some(Region::CnNorthwest1)},
+            _               =>  {None}
+    }
+}
+
+    pub fn list_tables_region(region: Region) -> () {
+    let client = DynamoDbClient::simple(region);
     let list_tables_input: ListTablesInput = Default::default();
 
     match client.list_tables(&list_tables_input).sync() {
         Ok(output) => {
             match output.table_names {
                 Some(table_name_list) => {
-                    println!("Tables in databse:");
+                    println!("Tables in database:"); 
 
                     for table_name in table_name_list {
                         println!("{}", table_name);
@@ -75,6 +98,6 @@ mod test {
         println!("Function Output:\n");
         tables::list_tables_default();
         println!("\n\nExpected output:\n");
-        println!("Tables in databse:\ncredential-store");
+        println!("Tables in database:\ncredential-store");
     }
 }
