977dfc857835e95f885fda1d2aff9031f10a31ab
Unknown <cromwellj@sou.edu>
2018-04-21 08:50:11 -0700

    Create Table Format
    Fixed formatting issues with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 extern crate rusoto_core;
-extern crate rusoto_dynamodb;
 extern crate rusoto_credential;
+extern crate rusoto_dynamodb;
 
 use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
 use rusoto_dynamodb::*;
-
-
 
 #[macro_export]
 macro_rules! attributes {
@@ -13,7 +11,8 @@ macro_rules! attributes {
         {
             let mut temp_vec = Vec::new();
             $(
-                temp_vec.push(AttributeDefinition { attribute_name: String::from($val), attribute_type: String::from($attr_type) });
+                temp_vec.push(AttributeDefinition { attribute_name: String::from($val),
+                                attribute_type: String::from($attr_type) });
             )*
             temp_vec
         }
@@ -26,7 +25,8 @@ macro_rules! key_schema {
         {
             let mut temp_vec = Vec::new();
             $(
-                temp_vec.push(KeySchemaElement { key_type: String::from($key_type), attribute_name: String::from($name) });
+                temp_vec.push(KeySchemaElement { key_type: String::from($key_type),
+                                attribute_name: String::from($name) });
             )*
             temp_vec
         }
@@ -57,9 +57,6 @@ macro_rules! val {
 	);
 }
 
-
-
-
 pub mod tables {
     use rusoto_core::region::Region;
     use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
@@ -70,86 +67,81 @@ pub mod tables {
         let list_tables_input: ListTablesInput = Default::default();
 
         match client.list_tables(&list_tables_input).sync() {
-            Ok(output) => {
-                match output.table_names {
-                    Some(table_name_list) => {
-                        println!("Tables in database:");
-
-                        for table_name in table_name_list {
-                            println!("{}", table_name);
-                        }
-                    },
-                    None => println!("No tables in database!"),
-                }
-            },
-            Err(error) => {
-                println!("Error: {:?}", error);
-            },
-        }
-    }
-
-    pub fn get_region(reg: &str) -> Option<Region> {
-        match reg {
-            "apnortheast1" =>  {return Some(Region::ApNortheast1)},
-            "apnortheast2" =>  {return Some(Region::ApNortheast2)},
-            "apsout1"      =>  {return Some(Region::ApSouth1)},
-            "apsoutheast1" =>  {return Some(Region::ApSoutheast1)},
-            "apsoutheast2" =>  {return Some(Region::ApSoutheast2)},
-            "cacentral1"   =>  {return Some(Region::CaCentral1)},
-            "eucentral1"   =>  {return Some(Region::EuCentral1)},
-            "euwest1"      =>  {return Some(Region::EuWest1)},
-            "euwest2"      =>  {return Some(Region::EuWest2)},
-            "euwest3"      =>  {return Some(Region::EuWest3)},
-            "saeast1"      =>  {return Some(Region::SaEast1)},
-            "useast1"      =>  {return Some(Region::UsEast1)},
-            "useast2"      =>  {return Some(Region::UsEast2)},
-            "uswest1"      =>  {return Some(Region::UsWest1)},
-            "uswest2"      =>  {return Some(Region::UsWest2)},
-            "usgovwest1"   =>  {return Some(Region::UsGovWest1)},
-            "cnnorth1"     =>  {return Some(Region::CnNorth1)},
-            "cnnorthwest1" =>  {return Some(Region::CnNorthwest1)},
-            _               =>  {None}
-    }
-}
-
-    pub fn list_tables_region(region: Region) -> () {
-    let client = DynamoDbClient::simple(region);
-    let list_tables_input: ListTablesInput = Default::default();
-
-    match client.list_tables(&list_tables_input).sync() {
-        Ok(output) => {
-            match output.table_names {
+            Ok(output) => match output.table_names {
                 Some(table_name_list) => {
                     println!("Tables in database:");
 
                     for table_name in table_name_list {
                         println!("{}", table_name);
                     }
-                },
+                }
                 None => println!("No tables in database!"),
+            },
+            Err(error) => {
+                println!("Error: {:?}", error);
             }
-        },
-        Err(error) => {
-            println!("Error: {:?}", error);
         }
     }
+
+    pub fn get_region(reg: &str) -> Option<Region> {
+        match reg {
+            "apnortheast1" => return Some(Region::ApNortheast1),
+            "apnortheast2" => return Some(Region::ApNortheast2),
+            "apsout1" => return Some(Region::ApSouth1),
+            "apsoutheast1" => return Some(Region::ApSoutheast1),
+            "apsoutheast2" => return Some(Region::ApSoutheast2),
+            "cacentral1" => return Some(Region::CaCentral1),
+            "eucentral1" => return Some(Region::EuCentral1),
+            "euwest1" => return Some(Region::EuWest1),
+            "euwest2" => return Some(Region::EuWest2),
+            "euwest3" => return Some(Region::EuWest3),
+            "saeast1" => return Some(Region::SaEast1),
+            "useast1" => return Some(Region::UsEast1),
+            "useast2" => return Some(Region::UsEast2),
+            "uswest1" => return Some(Region::UsWest1),
+            "uswest2" => return Some(Region::UsWest2),
+            "usgovwest1" => return Some(Region::UsGovWest1),
+            "cnnorth1" => return Some(Region::CnNorth1),
+            "cnnorthwest1" => return Some(Region::CnNorthwest1),
+            _ => None,
+        }
+    }
+
+    pub fn list_tables_region(region: Region) -> () {
+        let client = DynamoDbClient::simple(region);
+        let list_tables_input: ListTablesInput = Default::default();
+
+        match client.list_tables(&list_tables_input).sync() {
+            Ok(output) => match output.table_names {
+                Some(table_name_list) => {
+                    println!("Tables in database:");
+
+                    for table_name in table_name_list {
+                        println!("{}", table_name);
+                    }
+                }
+                None => println!("No tables in database!"),
+            },
+            Err(error) => {
+                println!("Error: {:?}", error);
+            }
+        }
     }
 }
-
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn t_authenticate_user(){
+    fn t_authenticate_user() {
         // Set AWS Variables to NULL values originally
 
         // Run Authenticate User method
 
         // Assert equality of AWS environment
         // vars with their expected values
-        assert_eq!(1,1);
+        assert_eq!(1, 1);
     }
 
     #[test]
